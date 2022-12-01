ab7531e3649b58f748f370f87ec21da27d4c768e
JMBrands <74452799+JMBrands@users.noreply.github.com>
2022-12-01 10:01:27 +0100

    added queue

--- jukebox.ino
+++ jukebox.ino
@@ -5,6 +5,7 @@ SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
 
 int buttonPins[16] = {19,18,17,16,15,14,2,3,4,5,6,7,8,9,12,13};
+int queue[2] = 0;
 
 void setup() {
   mySoftwareSerial.begin(9600);      
@@ -22,13 +23,21 @@ void setup() {
 
 
 void loop() {
-  if (digitalRead(0)== HIGH){
     for (int i=0; i<8; i++) 
       if (digitalRead(buttonPins[i]) == LOW) {
         for (int j=8; j<16; j++)
           if (digitalRead(buttonPins[j]) == LOW) {
-            myDFPlayer.playMp3Folder(i*10+(j-7)); 
-            delay(200);
+            if queue[0] == 0 {
+              queue[1] = i*10+(j+1);
+              }
+            else {
+              queue[0] = i*10+(j+1);
+              }
+            if (digitalRead(0)== HIGH){
+              myDFPlayer.playMp3Folder(queue[0]); 
+              delay(200);
+              queue[0] = queue[1];
+              queue[1] = 0;
           }
       }
   } 
